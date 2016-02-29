cbf4bc74b726f732d0bf82a2f60b729b1942a992
Saem <ottosigma@gmail.com>
2016-03-01 01:34:50 +0200

    Initial commit

---
+++ sameperson/Main.java
@@ -0,0 +1,12 @@
+package com.sameperson;
+
+import com.sameperson.logic.Riddler;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        new Riddler(20).run();
+
+    }
+}
---
+++ sameperson/logic/Riddler.java
@@ -0,0 +1,56 @@
+package com.sameperson.logic;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Riddler {
+
+    private int riddledNumber;
+    private int guess;
+
+    private Scanner reader;
+    private Random random;
+
+    public Riddler(int number) {
+        this.random = new Random();
+        this.riddledNumber = random.nextInt(number + 1);
+        this.reader = new Scanner(System.in);
+    }
+
+    public void run() {
+        String input;
+        while(true) {
+            System.out.println("Enter your number between 1 and 20: ");
+            input = reader.nextLine();
+            if(input.equals("q")) {
+                System.out.println("The game has been interrupted. Bye!");
+                break;
+            }
+            try {
+                guess = Integer.parseInt(input);
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Not a number!");
+                continue;
+            }
+            if(validateGuess(guess) < 0) {
+                break;
+            }
+        }
+    }
+
+    private int validateGuess(int guess) {
+        if(guess == this.riddledNumber) {
+            System.out.println("Right guess!");
+            return -1;
+        } else if(guess > this.riddledNumber) {
+            System.out.println("Riddled number is lower");
+            return 1;
+        } else if(guess < this.riddledNumber) {
+            System.out.println("Riddled number is higher");
+            return 1;
+        }
+        System.out.println("Unable to process a guess, try again!");
+        return 1;
+    }
+}
